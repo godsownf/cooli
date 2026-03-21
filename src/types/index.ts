1971a94fe155e783b15f923da0c2df64490f35c8
magit notif <231088448+godsownf@users.noreply.github.com>
2026-03-20 21:53:12 -0400

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,16 +1,18 @@
-export interface Cookie {
-  id: string;
-  name: string;
-  value: string;
-  domain: string;
-  path: string;
-  expiry: string;
-  secure: boolean;
-  httpOnly: boolean;
-}
+export interface 
+  Cookie {
+    id: string;
+    name: string;
+    value: string;
+    domain: string;
+    path: string;
+    expiry: string;
+    secure: boolean;
+    httpOnly: boolean;
+  }
 
-export interface SessionBackup {
-  timestamp: string;
-  cookies: Cookie[];
-  userAgent: string;
-}
+export interface 
+  SessionBackup {
+    timestamp: string;
+    cookies: Cookie[];
+    userAgent: string;
+  }
